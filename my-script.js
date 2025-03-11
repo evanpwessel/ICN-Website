35adaacc83a87902af2fa8b6604d52948871220a
Evan Wessel <epwess@gmail.com>
2025-03-11 18:20:20 -0400

    Small edits
    Added my-script.js and deleted line in styles

---
+++ my-script.js
@@ -0,0 +1,23 @@
+console.log("Suh Dude")
+
+let names = "Evan"
+let surname = "Wessel"
+
+console.log(names);
+console.log(surname);
+
+console.log((4 + 6 + 9) / 77)
+
+let a = 10
+console.log(a)
+a = 12
+console.log(a)
+
+let b = a * 2
+console.log(b)
+
+const max = 57
+const actual = max - 13
+const percentage = actual / max
+console.log(percentage)
+
